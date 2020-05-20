0a0b1e37d08ad939a33d8073587badb5101db2fa
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2020-05-20 12:05:32 +0200

    Working on Roundmanager Tests

--- src/test/scala/de.htwg.sa.dominion/model/roundmanagerComponent/RoundmanagerSpec.scala
+++ src/test/scala/de.htwg.sa.dominion/model/roundmanagerComponent/RoundmanagerSpec.scala
@@ -5,16 +5,34 @@ import org.scalatest.{Matchers, WordSpec}
 
 class RoundmanagerSpec extends WordSpec with Matchers {
   val handLuca: List[Card] = List(Cards.copper, Cards.copper, Cards.copper, Cards.copper, Cards.copper)
-  val Luca8 = Player("Luca", 0, Nil, handLuca, Nil, 1, 1, 0)
-  val Luca = Player("Luca", 0, Nil, Nil, handLuca, 1, 1, 0)
+  val handLuca1: List[Card] = List(Cards.village,Cards.village,Cards.village,Cards.village,Cards.village)
+  val Luca8 = Player("Luca", 0, Nil, handLuca1, Nil, 1, 1, 5)
+  val Luca = Player("Luca", 0, Nil, handLuca1, Nil, 1, 1, 0)
+  val copper: List[Card] = List(Cards.copper, Cards.copper)
+  val silver: List[Card] = List(Cards.silver,Cards.silver)
+  val decks: List[List[Card]] = List(copper,silver)
   val playerList: List[Player] = List(Luca,Luca8)
-  val roundmanager: Roundmanager = Roundmanager (Nil, Nil, 0, 1, Nil, 0, gameEnd = false, Nil,
-    RoundmanagerStatus.PLAY_CARD_PHASE, 0, Nil)
+  val roundmanager: Roundmanager = Roundmanager (playerList, Nil, 2, 1, Nil, 0, gameEnd = false, Nil,
+    RoundmanagerStatus.PLAY_CARD_PHASE, 1, Nil)
+  val roundmanagervalidateBuySelect: Roundmanager = Roundmanager (playerList, Nil, 2, 1, decks, 0, gameEnd = false, Nil,
+    RoundmanagerStatus.PLAY_CARD_PHASE, 1, Nil)
   //Hurensohn
   "A Player" when {
     "new" should {
       "have a checkifBuyLeft method" in {
        roundmanager.checkIfBuyLeft(playerList) should be (true)
+      }
+      "have a validateBuySelectInput method" in {
+        roundmanager.validateBuySelectInput("0") should be (false)
+        roundmanagervalidateBuySelect.validateBuySelectInput("1") should be (true)
+      }
+      "have a validateHandSelectInputActionCard method" in {
+        //roundmanagervalidateBuySelect.copy(playerTurn = 0).validateHandSelectInputActionCard("4") should be (true)
+        roundmanagervalidateBuySelect.validateHandSelectInputActionCard("") should be (false)
+      }
+      "have a validateWorkshopInputForPlayingDecks method" in {
+        roundmanagervalidateBuySelect.validateWorkshopInputForPlayingDecks("1") should not be (true)
+        roundmanagervalidateBuySelect.validateWorkshopInputForPlayingDecks("") should be (false)
       }
     }
   }
