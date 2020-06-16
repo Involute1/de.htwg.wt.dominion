dce953d858f9ef2bbab2f10b3ccb0ae759e38e40
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2020-06-16 18:55:46 +0200

    MongoPlayer

--- playerModule/src/main/scala/de/htwg/sa/dominion/model/playerDatabaseComponent/mongoImpl/PlayerMongoDbDAO.scala
+++ playerModule/src/main/scala/de/htwg/sa/dominion/model/playerDatabaseComponent/mongoImpl/PlayerMongoDbDAO.scala
@@ -1,14 +1,25 @@
 package de.htwg.sa.dominion.model.playerDatabaseComponent.mongoImpl
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.stream.ActorMaterializer
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import de.htwg.sa.dominion.model.playerComponent.playerBaseImpl.Player
 import de.htwg.sa.dominion.model.playerDatabaseComponent.IPlayerDatabase
 import de.htwg.sa.dominion.util.DatabasePlayer
 import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
 import play.api.libs.json.Json
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.util.Try
 
-class PlayerMongoDbDAO extends IPlayerDatabase {
+class PlayerMongoDbDAO extends IPlayerDatabase with PlayJsonSupport {
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
   val uri: String = "mongodb+srv://dominionUser:dominion@dominioncluster-fnmjl.mongodb.net/Dominion?retryWrites=true&w=majority"
   System.setProperty("org.mongodb.async.type", "netty")
   val client: MongoClient = MongoClient(uri)
@@ -30,11 +41,13 @@ class PlayerMongoDbDAO extends IPlayerDatabase {
 
   override def update(playerList: List[Player]): Boolean = {
     try {
-
-      val dbPlayer: List[DatabasePlayer] = List(playerList.foreach(x => DatabasePlayer(x.name, x.value, x.actions, x.buys, x.money)))
-      val playerDoc: Document = Document(Json.prettyPrint(Json.toJson(dbPlayer)))
-      playerCollection.insertOne(playerDoc).head()
-
+      for (doc <- playerCollection.find()) playerCollection.deleteMany(doc).head()
+      for (player <- playerList) {
+        val dbPlayer = DatabasePlayer(player.name, player.value, player.actions, player.buys, player.money)
+        val playerDoc: Document = Document(Json.prettyPrint(Json.toJson(dbPlayer)))
+        playerCollection.insertOne(playerDoc).head()
+        Http().singleRequest(Get("http://0.0.0.0:8082/card/save", (player.handCards, player.stacker, player.deck, player.value)))
+      }
       true
     } catch {
       case error: Error =>
@@ -43,5 +56,14 @@ class PlayerMongoDbDAO extends IPlayerDatabase {
     }
   }
 
-  override def delete: Boolean = ???
+  override def delete: Boolean = {
+    try {
+      for (doc <- playerCollection.find()) playerCollection.deleteMany(doc).head()
+      true
+    } catch {
+      case error: Error =>
+        println("Database error: ", error)
+        false
+    }
+  }
 }
